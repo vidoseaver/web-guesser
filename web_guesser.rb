0bd841ca8c0fe584753c220e7a972b5fb807b5f6
Vido <vidoseaver@gmail.com>
2016-09-28 16:14:23 -0600

    done until extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,6 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
+# require './views/index'
+ NUMBER = rand(10)
 
 get '/' do
-  "The SECRET NUMBER is #{rand(100)}"
+  guess = params["guess"]
+  message = check_guess(guess.to_i)
+  erb :index, :locals => { guess: guess, message: message}
+end
+
+def check_guess(guess)
+  case
+  when (guess + 5) < NUMBER
+    "way too low"
+  when (guess - 5) > NUMBER
+    "way too high"
+  when guess > NUMBER
+    "too high"
+  when guess < NUMBER
+    "too low"
+  when guess == NUMBER
+    "nailed it the secret number is #{NUMBER}"
+  else
+    ""
+  end
 end
